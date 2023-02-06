24aa569c5ab36043a07faf0fa1b9170e3da5aba2
GuysRGithub <guysdeveloperrobot@gmail.com>
2023-02-06 09:16:39 +0700

    Use state context for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,13 @@ import { Link, useNavigate } from 'react-router-dom';
 import { CustomButton } from './';
 import { logo, menu, search, thirdweb } from '../assets';
 import { navlinks } from '../constants';
+import { useStateContext } from '../context';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState('dashboard');
   const [toggleDrawer, setToggleDrawer] = useState(false);
-  const address = null;
+  const {connect, address} = useStateContext();
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
@@ -83,7 +84,7 @@ const Navbar = () => {
               styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
               handleClick={() => {
                 if(address) navigate('create-campaign')
-                else 'connect()';
+                else connect();
               }}
             />
             </div>
